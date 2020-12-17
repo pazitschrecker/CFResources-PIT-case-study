ad6fd4658c86e68f7dd61eb78b0078ce17343d49
Pazit <pazitrose1999@gmail.com>
2020-12-17 18:41:43 -0500

    Added navigation between tabs and alt text for all images

--- index.js
+++ index.js
@@ -25,4 +25,39 @@ $(document).ready(function () {
     $("#uj4").addClass("active");
     console.log("showing uj4");
   });
+  $("cf-process").click(function () {
+    $("#cf-process").show();
+    $("#cf-process").addClass("active");
+    console.log("showing cf-process");
+  });
+
+  $("mind-maps").click(function () {
+    $("#mind-maps").show();
+    $("#mind-maps").addClass("active");
+    console.log("showing mind-maps");
+  });
+
+  $("affinity-map").click(function () {
+    $("#affinity-map").show();
+    $("#affinity-map").addClass("active");
+    console.log("showing affinity-map");
+  });
+
+  $("lo-fi").click(function () {
+    $("#lo-fi").show();
+    $("lo-fi").addClass("active");
+    console.log("showing lo-fi");
+  });
+
+  $("mid-fi").click(function () {
+    $("#mid-fi").show();
+    $("mid-fi").addClass("active");
+    console.log("showing mid-fi");
+  });
+
+  $("hi-fi").click(function () {
+    $("#hi-fi").show();
+    $("hi-fi").addClass("active");
+    console.log("showing hi-fi");
+  });
 });
