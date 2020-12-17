b5b95117d8041387fe5187b5ef38423821d2fef5
Pazit <pazitrose1999@gmail.com>
2020-12-17 16:02:49 -0500

    Added nav tabs, more text

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
   $("#navbar-phases").click(function () {
     $("#phases-dropdown").toggle();
   });
+  $("#li.tab1").addClass("active");
+  $("templates-prototype").addClass("active");
 });
