3130eb45e4f607c154cb7a05f072b7a684fc0660
Pazit <pazitrose1999@gmail.com>
2020-12-17 17:11:57 -0500

    Fixed nav tabs

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ $(document).ready(function () {
   });
   $("#li.tab1").addClass("active");
   $("templates-prototype").addClass("active");
+
+  $("uj1-tab").click(function () {
+    $("#uj1").show();
+  });
+  $("uj2-tab").click(function () {
+    $("#uj2").show();
+  });
+  $("uj3-tab").click(function () {
+    $("#uj3").show();
+  });
+  $("uj4-tab").click(function () {
+    $("#uj4").show();
+  });
 });
